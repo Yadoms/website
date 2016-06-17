1e7b346145adbed68d1d44ef121bf211fce86230
nhilaire <nhilaire@nse-groupe.com>
2016-06-17 14:22:48 +0200

    added discover page header and footer method used to factorize code

---
+++ js/yadoms.js
@@ -0,0 +1,20 @@
+﻿
+$(function () {
+    $("#header").load("header.html", function () {
+        //small debounce
+        setTimeout(function () {
+            //we set the right active item in the main menu
+            if (window.location.pathname.indexOf("index.html") > -1)
+                $("#mainMenu li.index").addClass("active");
+
+            if (window.location.pathname.indexOf("discover.html") > -1)
+                $("#mainMenu li.discover").addClass("active");
+        },
+            100);
+    });
+
+    $("#footer").load("footer.html");
+});
+
+
+
