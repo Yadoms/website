14e945292b8fbf5d82b164f21b43575ef7c8dc2b
Sébastien GALLOU <sebastien.gallou@xerox.com>
2016-09-22 11:51:19 +0200

    Fix listing of components to update

--- downloads/update/plugins.php
+++ downloads/update/plugins.php
@@ -4,54 +4,14 @@ require('helper.inc');
 ?>
 
 <?
-//getting parameters
-$os = $_GET["os"];
-$arch = $_GET["arch"];
-$lang = $_GET["lang"];
-$devMode = $_GET["devMode"];
-
-$answer = array( 'inputParameters' => array('os' => $os, 'arch' => $arch, 'devMode' => $devMode));
-
-if (($os == null) || ($arch == null))
-	sendAnswerAndExit(false, "os and arch GET parameters must be defined", $answer);
-
-//we manage param tolerance
-$os = matchInArray($os, $osWin, "windows");
-$os = matchInArray($os, $osLinux, "linux");
-$os = matchInArray($os, $osMac, "darwin");
-
-$arch = matchInArray($arch, $archX86, "x86");
-$arch = matchInArray($arch, $archX64, "x64");
-
-//we look for all directories located in /plugins/$os/$arch/
-	
-if (isset ($devMode)){
-   $dirName = "dev/";   
-}
-else{
-   $dirName = "";
-}
-
-$dirName .= "plugins/" . $os;
-
-if (!is_dir($dirName))
-	sendAnswerAndExit(false, "Folder not found : " . $dirName, $answer);
-
-$dirName .= "/" . $arch;
-
-if (!is_dir($dirName))
-	sendAnswerAndExit(false, "arch folder not found :" . $dirName, $answer);
-
-$dirName .= '/';
 
 try
 {
-	$answer["plugins"] = buildItemList($dirName, $lang);
+   sendAnswerAndExit(true, "", listNativeSubPackages("plugins", $_GET["os"], $_GET["arch"], $_GET["lang"], $_GET["devMode"]));
 }
 catch (Exception $e)
 {
-	sendAnswerAndExit(false, $e->getMessage(), $answer);
+   sendAnswerAndExit(false, $e->getMessage(), $answer);
 }
 
-sendAnswerAndExit(true, "", $answer);
 ?>
