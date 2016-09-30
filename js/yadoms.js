4252b6470f59d3b61389b23c1d01d901d5d0a11e
Nicolas HILAIRE <nhilaire@nse-groupe.com>
2016-09-30 15:55:00 +0200

    added download page first page have been made responsive

--- js/yadoms.js
+++ js/yadoms.js
@@ -9,6 +9,9 @@ $(function () {
 
             if (window.location.pathname.indexOf("discover.html") > -1)
                 $("#mainMenu li.discover").addClass("active");
+
+            if (window.location.pathname.indexOf("download.html") > -1)
+                $("#mainMenu li.download").addClass("active");
         },
             100);
     });
