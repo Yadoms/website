a1ee3c53a321b05161394a3f8435cb0e88a56f99
Nicolas HILAIRE <nhilaire@nse-groupe.com>
2017-01-25 14:28:17 +0100

    switch of image with language has been refunded to de more generic and is now usable in all pages

--- js/yadoms.js
+++ js/yadoms.js
@@ -20,7 +20,28 @@ $(function () {
     });
 
     $("#footer").load("footer.html");
+    
 });
 
+/**
+   This function aims to help to have different image lodaded based on the language of the navigator
+   <img class="translated-file-attribute" translated-attr="src" translated-attr-value="img/screenshots/{language}/1.png"/>
+   will become
+   <img class="translated-file-attribute" translated-attr="src" translated-attr-value="img/screenshots/{language}/1.png" src="img/screenshots/en/1.png"/>
+**/
+
+function updateTranlatedFileAttributes(langugageFolder) {
+      var itemsToTranslate = $(".translated-file-attribute");
+      $.each(itemsToTranslate, function (index, value) {
+         var $value = $(value);
+         //we get attr to translate
+         var attrName = $value.attr("translated-attr");
+         //we get the filename and replace {language} by the right value
+         var realFilename = $value.attr("translated-attr-value");
+         realFilename = realFilename.replace("{language}", langugageFolder);
+         //we update the attr with the new value
+         $value.attr(attrName, realFilename);
+      });
+   }
 
 
