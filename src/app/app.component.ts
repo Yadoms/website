e4f1238a14e57e5aa0117f7fd2734e45ecf986d3
oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-05-27 02:17:07 +0200

    add APP_BASE_HREF provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, ViewEncapsulation } from "@angular/core";
-
+import { APP_BASE_HREF } from "@angular/common";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
   encapsulation: ViewEncapsulation.None,
+  providers: [{ provide: APP_BASE_HREF, useValue: "/new" }],
 })
 export class AppComponent {
   title = "yadoms";
