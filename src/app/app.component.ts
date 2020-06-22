82214a021ac02e5701441d72c3b6ac09ed569b3e
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2020-06-22 09:55:54 +0200

    try to fix assets path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component, ViewEncapsulation } from "@angular/core";
-import { APP_BASE_HREF } from "@angular/common";
+import { Component, ViewEncapsulation } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None,
-  providers: [{ provide: APP_BASE_HREF, useValue: "/new" }],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/new' }],
 })
 export class AppComponent {
-  title = "yadoms";
+  title = 'yadoms';
 }
