fce98ec12c487a3d4161c413f1f379adbed5db34
oussama DAHMAZ <oussama.dahmaz@gmail.com>
2021-02-16 22:18:26 +0100

    #5 add LanguageDetector & Backend

--- src/js/i18.js
+++ src/js/i18.js
@@ -1,30 +1,26 @@
 import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
 
 export default class I18 {
     constructor() {
         console.log('init i18next');
         this.init();
     }
-    init(){
-        i18n.init({
-            lng: 'en',
-            debug: true,
-            resources: {
-              en: {
-                translation: {
-                  "key": "The ultimate home automation solution"
-                }
-              },    
-              fr: {
-                translation: {
-                  "key": "La solution domotique ultime"
-                }
-              }
-            }
-          }, function(err, t) {
-            // initialized and ready to go!
-            document.getElementById('output').innerHTML = i18n.t('key');
-          });
-    }
     
+    init(){ i18n.use(LanguageDetector)
+                .use(Backend)
+                .init({
+                    fallbackLng: "en", 
+                    useDataAttrOptions: true, 
+                    debug: true, 
+                    backend: {
+                        loadPath: '/locales/{{lng}}.json'
+                    }
+                })
+                .then(function(t) {
+                    document.getElementById('output').innerHTML = i18n.t('title.ultimate');
+                }
+            );
+    }  
 }
