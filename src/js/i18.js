7f9fe797df30be18b3010fb34a69632c5e40cd38
oussama DAHMAZ <oussama.dahmaz@gmail.com>
2021-02-16 22:20:51 +0100

    # disable i18next debug mode

--- src/js/i18.js
+++ src/js/i18.js
@@ -13,7 +13,7 @@ export default class I18 {
                 .init({
                     fallbackLng: "en", 
                     useDataAttrOptions: true, 
-                    debug: true, 
+                    debug: false, 
                     backend: {
                         loadPath: '/locales/{{lng}}.json'
                     }
