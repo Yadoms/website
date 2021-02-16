8433e10abcc43244a7f8d192c00f574aa63236d9
oussama DAHMAZ <oussama.dahmaz@gmail.com>
2021-02-16 23:17:42 +0100

    #5 add jqueryI18next

--- src/js/i18.js
+++ src/js/i18.js
@@ -1,6 +1,7 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
+import jqueryI18next from 'jquery-i18next';
 
 export default class I18 {
     constructor() {
@@ -8,19 +9,21 @@ export default class I18 {
         this.init();
     }
     
-    init(){ i18n.use(LanguageDetector)
+    init()
+    { 
+        i18n.use(LanguageDetector)
                 .use(Backend)
                 .init({
                     fallbackLng: "en", 
                     useDataAttrOptions: true, 
-                    debug: false, 
+                    debug: true, 
                     backend: {
                         loadPath: '/locales/{{lng}}.json'
                     }
-                })
-                .then(function(t) {
-                    document.getElementById('output').innerHTML = i18n.t('title.ultimate');
-                }
-            );
+                }, function(err, t) {
+                    jqueryI18next.init(i18n, $);
+                    $('body').localize();
+                  });
+        
     }  
 }
