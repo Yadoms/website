b26173a27865c89f2d5994805ba972d7cb707b8a
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2021-04-04 14:43:12 +0200

    remove aos js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@
 import Navbar from "./js/navbar.js";
 import I18 from "./js/i18.js"
 import CoinAnimation from "./js/coinAnimation";
-import AosAnimation from "./js/aosAnimation";
 
 const navbar = new Navbar()
 const i18n = new I18()
 const coinAnimation = new CoinAnimation()
-const aosAnimation = new AosAnimation()
 
--- src/js/aosAnimation.js
+++
@@ -1,8 +0,0 @@
-import AOS from 'aos';
-
-export default class AosAnimation {
-    constructor() {
-        console.log('AosAnimation init');
-        AOS.init({disable: 'mobile'});
-    }
-}
