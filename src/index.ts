65decbb2673f8f039cd21094e97be0c60a86159a
oussama DAHMAZ <oussama.dahmaz@gmail.com>
2021-09-03 22:09:03 +0200

    add swipper to supported devices #27

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,44 @@
 import {getYadomsLatestRelease, LatestReleaseInfos} from "./lib/service";
 import {generateDownloadPage, generateResiliencePage} from "./lib/pages";
-import './tailwind.css';
 import Translator from '@andreasremdt/simple-translator';
+import Swiper, { Navigation, Pagination, Autoplay } from "swiper";
+import './tailwind.css';
 
+Swiper.use([Navigation, Pagination, Autoplay]);
+const swiper = new Swiper(".supportedDeviceSwiper", {
+    breakpoints: {
+        375: {
+            slidesPerView: 2,
+            spaceBetween: 10
+        },
+        // when window width is <= 499px
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 1
+        },
+        // when window width is <= 999px
+        999: {
+            slidesPerView: 6,
+            spaceBetween: 10
+        }
+    },
+    autoplay: {
+        delay: 1000,
+    },
+    pagination: {
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+});
 const translator = new Translator({
     defaultLanguage: "en",
     detectLanguage: true,
     selector: "[data-i18n]",
-    debug: true,
+    debug: false,
     registerGlobally: "__",
     persist: false,
     persistKey: "preferred_language",
